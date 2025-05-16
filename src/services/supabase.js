dc8efdaba8eaa075a3576dddf27e197993c78f56
Gabriel-Ionita <gbunica@gmail.com>
2025-05-16 22:28:48 +0300

    Resolve conflicts and finalize project setup

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://aexzaxwiuolrythxzjml.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFleHpheHdpdW9scnl0aHh6am1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczOTEwNDQsImV4cCI6MjA2Mjk2NzA0NH0.Sme3EoNLHXUqFYv3vOn24kb4rwLlQ1TIJusqnmkVmdY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
